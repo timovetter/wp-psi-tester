a9204d6344b6b683d3ac74f49fb57356b82aaeb3
Luca Bäuerle <mail@luca-baeuerle.de>
2022-03-20 15:49:43 +0100

    github issue creation

--- github.js
+++ github.js
@@ -2,23 +2,24 @@ import fetch from 'node-fetch';
 
 let base64 = require('base-64');
 
-const username = "";
-const token = "";
-const repo = "";
+const config = {
+    username: "",
+    token: "",
+    repo: "",
+}
 
-
-const createIssue = async(title, body) => {
-    const postBody= {
+const createIssue = async (title, body) => {
+    const postBody = {
         title,
         body
     }
-    return await fetch('https://api.github.com/repos/' + repo + '/issues', {
+    return await fetch('https://api.github.com/repos/' + config.repo + '/issues', {
         method: 'post',
         body: JSON.stringify(postBody),
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/vnd.github.v3+json',
-            'Authorization': 'Basic ' + base64.encode(username + ":" + token)
+            'Authorization': 'Basic ' + base64.encode(config.username + ":" + config.token)
         }
     })
 }
