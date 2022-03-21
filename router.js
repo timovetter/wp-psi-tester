59588105649f70515d4e42e0a2cb8e8d1834b8e8
mm <mail@m29.dev>
2022-03-21 10:28:29 +0100

    Added multiple PSI test runs in the Github issue

--- router.js
+++ router.js
@@ -16,32 +16,36 @@ router.post('/tests', async function (req, res) {
         res.sendStatus(400);
         return;
     }
+
     const promises = [];
     for (const test_url of body.urls) {
         const url = new URL(psi_url);
         url.searchParams.append('url', test_url);
         url.searchParams.append('key', pshConfig.variable("PSI_KEY", process.env.PSI_KEY));
-        promises.push(fetch(url.href).then(res => {
-            if (res.status === 200) return res.json()
-            res.text().then(
-                t => console.log(res.status, t)
-            )
-            return {}
-        })
-            .then(json => {
-                const audits = json.lighthouseResult.audits;
-                for (const a in audits) {
-                    delete audits[a].details;
-                }
-                delete json.lighthouseResult.categories.performance.auditRefs;
-                return {
-                    url: test_url,
-                    audits: audits,
-                    categories: json.lighthouseResult.categories,
-                }
-            }));
+        for (let i = 0; i < 3; i++) {
+            promises.push(fetch(url.href)
+                .then(res => {
+                    if (res.status === 200) return res.json()
+                    res.text()
+                        .then(t => console.log(res.status, t))
+                    return {}
+                })
+                .then(json => {
+                    const audits = json.lighthouseResult.audits;
+                    for (const a in audits) {
+                        delete audits[a].details;
+                    }
+                    delete json.lighthouseResult.categories.performance.auditRefs;
+                    return {
+                        url: test_url,
+                        audits: audits,
+                        categories: json.lighthouseResult.categories,
+                    }
+                }));
+        }
     }
-    const d = await Promise.all(promises).then(data => data)
+    let d = await Promise.all(promises).then(data => data)
+    d = groupData(d);
     let post = createPost(d);
 
     const r = await createIssue("Test Metrics | " + body.name, post).then(res => res.json());
@@ -52,6 +56,19 @@ router.post('/tests', async function (req, res) {
 });
 
 
+function groupData(data) {
+    const o = {};
+    for (const d of data) {
+        if (Object.keys(o).includes(d.url)) {
+            o[d.url].push(d);
+        } else {
+            o[d.url] = [];
+            o[d.url].push(d);
+        }
+    }
+    return o;
+}
+
 /*
 ## https://trunk-hy2b2li-rk425yy73kk4w.us-4.platformsh.site
 
@@ -61,24 +78,32 @@ Scores:
 **largest-contentful-paint:** 0.95
 **cumulative-layout-shift:** 0.93
 **total-byte-weight:** 0.99
+....
+### RAW JSON START
+....
+### RAW JSON END
  */
 function createPost(data) {
     let post = "";
-    for (const d of data) {
-        post = post.concat('## Test on:', d.url, '\n');
+    for (const url of Object.keys(data)) {
+        post = post.concat(`## Test on:`, url, '\n');
         post = post.concat('\n');
         post = post.concat('Scores: ', '\n');
-
-        for (const dElement of Object.keys(d.audits)) {
-            if (d.audits[dElement].score !== null ){
-                post = post.concat('\n', `**${dElement}:** ${d.audits[dElement].score}`);
+        if (data[url].length === 0) {
+            continue;
+        }
+        for (const dElement of Object.keys(data[url][0].audits)) {
+            if (data[url][0].audits[dElement].score !== null ){
+                post = post.concat('\n', `**${dElement}:** ${data[url][0].audits[dElement].score}`);
             }
         }
+        let i = 1;
+        for (const rawData of data[url]) {
+            post = post.concat(`\n\n### RAW JSON START: RUN ${i}\n`, )
+            post = post.concat('```JSON\n', JSON.stringify(rawData), '\n```');
+            i++;
+        }
     }
-
-    post = post.concat('\n\n### RAW JSON START\n')
-    post = post.concat('```JSON\n',JSON.stringify(data), '\n```');
-    post = post.concat('\n### RAW JSON END\n')
 
     return post;
 }
